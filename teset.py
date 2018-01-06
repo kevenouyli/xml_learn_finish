9c2af9118bbeb9d1bc45993a253a8435ead4e991
linli303161 <linli303161@126.com>
2018-01-07 01:06:05 +0800

    complete . but need fix the algorithm ,choose the right trail according to the shortest distance.

--- teset.py
+++ teset.py
@@ -1,3 +1,10 @@
+# -*- coding:GBK -*-
+import xml.etree.ElementTree as ET
+import re
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from  collections import namedtuple
 import numpy as npET
 from xml.etree import ElementTree
 # a=[[100,10],[2,14],[52,56],[4,72],[5,27],[6,32]]
@@ -6,6 +13,12 @@ from xml.etree import ElementTree
 # 	b=[]
 # 	for i in a:
 # 		b.append(i[1])
+tree= ET.parse('2017.xml')
+root = tree.getroot()
+tag1 = root.getchildren()[0]    #get document
+tag2 = tag1.getchildren()[1]
+tag3 = tag2.getchildren()
+print(len(tag3))
 # 	return	max(b)
 # d=get_max(a)
 #
@@ -16,31 +29,78 @@ from xml.etree import ElementTree
 # 	a+=1
 #
 # print(a)
-
-tree = ElementTree.parse('test.xml')  # åŠ è½½æŒ‡å®šæ–‡ä»¶
-root = tree.getroot()  # èŽ·å¾—æ ¹èŠ‚ç‚¹
-sub1=root.getchildren()[0]
-
-# åˆ›å»ºèŠ‚ç‚¹
-Placemark = ElementTree.Element('Placemark')  # Element(tag, attrib={}, **extra)
-name = ElementTree.SubElement(Placemark, 'name')  # SubElement(parent, tag, attrib={}, **extra)
-Style = ElementTree.SubElement(Placemark, 'Style')
-LineStyle = ElementTree.SubElement(Style, 'LineStyle')
-color = ElementTree.SubElement(LineStyle, 'color')
-color.text='7fffff80'
-width = ElementTree.SubElement(LineStyle, 'width')
-width.text='4'
-IconStyle = ElementTree.SubElement(Style, 'IconStyle')
-Icon= ElementTree.SubElement(IconStyle, 'Icon')
-MultiGeometry = ElementTree.SubElement(Placemark, 'MultiGeometry')
-Point = ElementTree.SubElement(MultiGeometry, 'Point')
-coordinates = ElementTree.SubElement(Point, 'coordinates')
-coordinates.text='112.32037530,23.25538530,0'
-LineString= ElementTree.SubElement(MultiGeometry, 'LineString')
-coordinates = ElementTree.SubElement(LineString, 'coordinates')
-coordinates.text='112.32037530,23.25538530,0 112.32037530,23.25538530,0 112.31950730,23.25570050,0'
-
-root.append(Placemark)
+#
+# tree = ElementTree.parse('test.xml')  # ¼ÓÔØÖ¸¶¨ÎÄ¼þ
+# root = tree.getroot()  # »ñµÃ¸ù½Úµã
+# sub1=root.getchildren()[0]
+#
+# # ´´½¨½Úµã
+# Placemark = ElementTree.Element('Placemark')  # Element(tag, attrib={}, **extra)
+# name = ElementTree.SubElement(Placemark, 'name')  # SubElement(parent, tag, attrib={}, **extra)
+# Style = ElementTree.SubElement(Placemark, 'Style')
+# LineStyle = ElementTree.SubElement(Style, 'LineStyle')
+# color = ElementTree.SubElement(LineStyle, 'color')
+# color.text='7fffff80'
+# width = ElementTree.SubElement(LineStyle, 'width')
+# width.text='4'
+# IconStyle = ElementTree.SubElement(Style, 'IconStyle')
+# Icon= ElementTree.SubElement(IconStyle, 'Icon')
+# MultiGeometry = ElementTree.SubElement(Placemark, 'MultiGeometry')
+# Point = ElementTree.SubElement(MultiGeometry, 'Point')
+# coordinates = ElementTree.SubElement(Point, 'coordinates')
+# coordinates.text='112.32037530,23.25538530,0'
+# LineString= ElementTree.SubElement(MultiGeometry, 'LineString')
+# coordinates = ElementTree.SubElement(LineString, 'coordinates')
+# coordinates.text='112.32037530,23.25538530,0 112.32037530,23.25538530,0 112.31950730,23.25570050,0'
+#
+# root.append(Placemark)
 
 # write(file, encoding="us-ascii", xml_declaration=None, default_namespace=None, method="xml")
-tree.write('updated.xml', encoding='utf-8')
+# tree.write('updated.xml', encoding='utf-8')
+
+
+
+
+
+
+# import xml.dom.minidom as xdm
+# domtree = xdm.parse("2017.xml")
+# root = domtree.documentElement
+# tag1=root.childNodes
+# tag2=tag1[1].getElementsByTagName('Folder')
+# tar_tag=tag2[3].childNodes[1]
+# print(tar_tag.childNodes[0].nodeValue)
+# tag2=tag1.childNodes[1]
+# tag3=tag2.childNodes[1]
+# print(tag3.childNodes[0].nodeValue)
+
+# print(tag2.childNodes[0].nodeValue)
+# impl = xml.dom.minidom.getDOMImplementation()
+# # ÉèÖÃ¸ù½áµãemps
+# dom = impl.createDocument(None, 'emps', None)
+# root = dom.documentElement
+# Placemark = dom.createElement('Placemark ')
+#
+# # Ôö¼ÓÊôÐÔ
+# employee.setAttribute("empno", "1111")
+# root.appendChild(employee)
+#
+# # ÉèÖÃ×Ó½áµã
+# # ename
+# nameE = dom.createElement('ename')
+# nameT = dom.createTextNode('½Ü¿Ë')
+# nameE.appendChild(nameT)
+# # ×Ó½ÚµãÌí¼ÓÊôÐÔ
+# nameE.setAttribute("lastname", "ke")
+#
+# employee.appendChild(nameE)
+# # age
+# nameE = dom.createElement('age')
+# nameT = dom.createTextNode('33')
+# nameE.appendChild(nameT)
+#
+# employee.appendChild(nameE)
+#
+# f = open('emplist.xml', 'w')  # wÌæ»»Îªa£¬×·¼Ó
+# dom.writexml(f, addindent=' ', newl='\n')
+# f.close()
--- xml_test.py
+++ xml_test.py
@@ -16,7 +16,8 @@ def tag_get(tag):
 	print(next_name.text)
 	bdz=bdz_re.match(next_name.text)
 	if bdz :
-		tag_target=next_folder.getchildren()[1]
+		# tag_target=next_folder.getchildren()[1]
+		tag_target=tag
 		return
 	else:
 		tag_get(next_folder)
@@ -66,7 +67,7 @@ def order_list(maxdist_p,line_f):
 
 
 ######################################      求最大距离点的位置
-def get_maxdist(list_vec_f):
+def get_maxdist(list_vec_f,line):
 	list=[]
 	for i in list_vec_f:
 		# print(line[i.max_p],line[i.root_p])
@@ -77,7 +78,7 @@ def get_maxdist(list_vec_f):
 	# print(line[max_p],line[list_vec[max_p].root_p])
 	# max_dist=get_dist_max(line[max_p],line[list_vec[max_p].max_p])
 	# print(max_dist)
-	max_p2=list_vec[max_p].max_p
+	max_p2=list_vec_f[max_p].max_p
 	return [max_p,max_p2]
 
 
@@ -125,80 +126,126 @@ def orderlist_line(order,line_f):
 		line_result.append(str(line_use[i].jd)+','+str(line_use[i].wd)+','+'0')
 	return  line_result
 
-######################################                        主程序
-Point=namedtuple('point',['id','jd','wd']) # 定义点（经度，纬度）
-point_mesg=namedtuple('vec',['root_p','max_p'])   #定义存放点（最小值，最大值）
-tag_target=None
-tree= ET.parse('2017.xml')
-root = tree.getroot()
-tag1 = root.getchildren()[0]    #get document
-tag2 = tag1.getchildren()[1]  #get  first_folder
-bdz_re=re.compile(r".*站\b")
-jd_list = []  # 存放经度
-wd_list = []  # 存放纬度
-line = []   #存放各点经纬度
-place=[]
-tag_get(tag2)
 
-for i in tag_target.iter("coordinates"):
-	place.append(i.text)
-fen = re.compile(',')
-
-for i in place:     #存放经纬度
-	total = fen.split(i)
-	line.append(Point(id=place.index(i),jd=float(total[0]),wd=float(total[1])))
-	jd_list.append(float(total[0]))
-	wd_list.append(float(total[1]))
 	
-# for i in range(len(line)):  ##画出各点
-# 	plt.figure(1)
-# 	plt.plot(line[i].jd,line[i].wd, 'bs')
+def get_jwd(tag_target_f):
+	jd_list = []  # 存放经度
+	wd_list = []  # 存放纬度
+	line = []  # 存放各点经纬度
+	place = []
+	Point = namedtuple('point', ['id', 'jd', 'wd'])  # 定义点（经度，纬度）
+	point_mesg = namedtuple('vec', ['root_p', 'max_p'])  # 定义存放点（原点，最大值）
+	fen = re.compile(',')
+	for i in tag_target_f.iter("coordinates"):
+		place.append(i.text)
+	for i in place:  # 存放经纬度
+		total = fen.split(i)
+		line.append(Point(id=place.index(i), jd=float(total[0]), wd=float(total[1])))
+		
+	list_vec = []  # 存放各点的最大、最小距离
+	for i in range(len(line)):
+		list_vec.append(point_mesg(root_p=i, max_p=get_pointplc(i, line)))
 	
-list_vec=[]   #存放各点的最大、最小距离
+	max_dist_point = get_maxdist(list_vec,line)
+	print(max_dist_point)
+	print(line)
+	list_r_order = order_list(max_dist_point, line)
+	right_list = orderlist_line(list_r_order, line)
+	str_right_list = ' '.join(right_list)
+	
+	return [str_right_list,right_list]
+	
+	
+	
+# for i in tag_target.iter("coordinates"):
+# 	place.append(i.text)
 
-for i in range(len(line)):
-	list_vec.append(point_mesg(root_p=i,max_p=get_pointplc(i,line)))
 
-print(list_vec)
-max_dist_point=get_maxdist(list_vec)
-print(max_dist_point)
-print(line)
-list_r_order=order_list(max_dist_point,line)
-# plt.figure(3)
-# plt.plot(jd_list,wd_list)    ##画出各点连线
-# draw(list_r_order,line)
-print(list_r_order)
-right_list=orderlist_line(list_r_order,line)
-print(right_list)
-str_right_list=' '.join(right_list)
-print (str_right_list)
+# Point=namedtuple('point',['id','jd','wd']) # 定义点（经度，纬度）
+# point_mesg=namedtuple('vec',['root_p','max_p'])   #定义存放点（原点，最大值）
+# jd_list = []  # 存放经度
+# wd_list = []  # 存放纬度
+# line = []   #存放各点经纬度
+# place=[]
+# fen = re.compile(',')
+# for i in place:     #存放经纬度
+# 	total = fen.split(i)
+# 	line.append(Point(id=place.index(i),jd=float(total[0]),wd=float(total[1])))
+# 	jd_list.append(float(total[0]))
+# 	wd_list.append(float(total[1]))
+#
+# # for i in range(len(line)):  ##画出各点
+# # 	plt.figure(1)
+# # 	plt.plot(line[i].jd,line[i].wd, 'bs')
+#
+# list_vec=[]   #存放各点的最大、最小距离
+#
+# for i in range(len(line)):
+# 	list_vec.append(point_mesg(root_p=i,max_p=get_pointplc(i,line)))
+#
+# print(list_vec)
+# max_dist_point=get_maxdist(list_vec)
+# print(max_dist_point)
+# print(line)
+# list_r_order=order_list(max_dist_point,line)
+# # plt.figure(3)
+# # plt.plot(jd_list,wd_list)    ##画出各点连线
+# # draw(list_r_order,line)
+# print(list_r_order)
+# right_list=orderlist_line(list_r_order,line)
+# print(right_list)
+# str_right_list=' '.join(right_list)
+# print (str_right_list)
 # plt.show()
 
 ########################################################################################################    加入xml节点
 #  1、使用etree 添加xml
-Placemark1 = ET.Element('Placemark')  # Element(tag, attrib={}, **extra)
-name1 = ET.SubElement(Placemark1,'name')  # SubElement(parent, tag, attrib={}, **extra)
-name1.text = tag_target.getchildren()[0].text
-Style1 = ET.SubElement(Placemark1,'Style')
-LineStyle1 = ET.SubElement(Style1,'LineStyle')
-color1 = ET.SubElement(LineStyle1,'color')
-color1.text = '7fffff80'
-width1 = ET.SubElement(LineStyle1,'width')
-width1.text = '4'
-IconStyle1 = ET.SubElement(Style1,'IconStyle')
-Icon1 = ET.SubElement(IconStyle1,'Icon')
-MultiGeometry1 = ET.SubElement(Placemark1,'MultiGeometry')
-Point1 = ET.SubElement(MultiGeometry1,'Point')
-coordinates1 = ET.SubElement(Point1,'coordinates')
-coordinates1.text=right_list[0]
-LineString1 = ET.SubElement(MultiGeometry1,'LineString')
-coordinates1 = ET.SubElement(LineString1,'coordinates')
-coordinates1.text = str_right_list
-
-tag_target.append(Placemark1)
-
-# write(file, encoding="us-ascii", xml_declaration=None, default_namespace=None, method="xml")
-tree.write('2017_updated.xml', encoding='utf-8')
+def xml_write(tag_target_f,right_list,str_right_list):
+	Placemark1 = ET.Element('Placemark')  # Element(tag, attrib={}, **extra)
+	name1 = ET.SubElement(Placemark1, 'name')  # SubElement(parent, tag, attrib={}, **extra)
+	name1.text = tag_target_f.getchildren()[0].text
+	Style1 = ET.SubElement(Placemark1, 'Style')
+	LineStyle1 = ET.SubElement(Style1, 'LineStyle')
+	color1 = ET.SubElement(LineStyle1, 'color')
+	color1.text = '7f400040'
+	width1 = ET.SubElement(LineStyle1, 'width')
+	width1.text = '8'
+	IconStyle1 = ET.SubElement(Style1, 'IconStyle')
+	Icon1 = ET.SubElement(IconStyle1, 'Icon')
+	MultiGeometry1 = ET.SubElement(Placemark1, 'MultiGeometry')
+	Point1 = ET.SubElement(MultiGeometry1, 'Point')
+	coordinates1 = ET.SubElement(Point1, 'coordinates')
+	coordinates1.text = right_list[0]
+	LineString1 = ET.SubElement(MultiGeometry1, 'LineString')
+	coordinates1 = ET.SubElement(LineString1, 'coordinates')
+	coordinates1.text = str_right_list
+	# tag_target_f.append(Placemark1)
+	# tree.write('2017_updated.xml', encoding='utf-8')
+	return  Placemark1
+	
+# Placemark1 = ET.Element('Placemark')  # Element(tag, attrib={}, **extra)
+# name1 = ET.SubElement(Placemark1,'name')  # SubElement(parent, tag, attrib={}, **extra)
+# name1.text = tag_target.getchildren()[0].text
+# Style1 = ET.SubElement(Placemark1,'Style')
+# LineStyle1 = ET.SubElement(Style1,'LineStyle')
+# color1 = ET.SubElement(LineStyle1,'color')
+# color1.text = '7fffff80'
+# width1 = ET.SubElement(LineStyle1,'width')
+# width1.text = '4'
+# IconStyle1 = ET.SubElement(Style1,'IconStyle')
+# Icon1 = ET.SubElement(IconStyle1,'Icon')
+# MultiGeometry1 = ET.SubElement(Placemark1,'MultiGeometry')
+# Point1 = ET.SubElement(MultiGeometry1,'Point')
+# coordinates1 = ET.SubElement(Point1,'coordinates')
+# coordinates1.text=right_list[0]
+# LineString1 = ET.SubElement(MultiGeometry1,'LineString')
+# coordinates1 = ET.SubElement(LineString1,'coordinates')
+# coordinates1.text = str_right_list
+#
+# tag_target.append(Placemark1)
+#
+# # write(file, encoding="us-ascii", xml_declaration=None, default_namespace=None, method="xml")
+# tree.write('2017_updated.xml', encoding='utf-8')
 # # print(order_list(line))
 
 
@@ -223,7 +270,36 @@ tree.write('2017_updated.xml', encoding='utf-8')
 #
 #
 
-	
+######################################                        主程序
+
+tag_target=None                     #######        解析xml
+tree= ET.parse('2017.xml')
+root = tree.getroot()
+tag1 = root.getchildren()[0]    #get document
+tag2 = tag1.getchildren()[1]  #get  first_folder
+tag3 = tag2.getchildren()[1:]
+
+# bdz_re=re.compile(r".*站\b")
+# tag_get(tag2)
+# bdz_line_tag=tag_target.getchildren()[1:]
+# print(bdz_line_tag)
+for bdz_tag in tag3:
+	bdz_line_tag=bdz_tag.getchildren()[1:]
+	for line_tag in bdz_line_tag:
+		line_tag_list = line_tag.getchildren()[1:]
+		for base_line in line_tag_list:
+			result_list = get_jwd(base_line)
+			place_mark = xml_write(base_line, result_list[1], result_list[0])
+			base_line.append(place_mark)
+
+
+# for line_tag in bdz_line_tag:
+# 	line_tag_list=line_tag.getchildren()[1:]
+# 	for base_line in line_tag_list:
+# 		result_list=get_jwd(base_line)
+# 		place_mark=xml_write(base_line,result_list[1],result_list[0])
+# 		base_line.append(place_mark)
+tree.write('2017_updated.xml', encoding='utf-8')
 
 
 
